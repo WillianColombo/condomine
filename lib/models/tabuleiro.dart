92fc8bd66293dc4c30bb6f3fd02d7f3806a3f9e8
Willian Colombo <williancolombo511@gmail.com>
2023-05-22 22:05:09 -0300

    Ajustes tabuleiro

--- lib/models/tabuleiro.dart
+++ lib/models/tabuleiro.dart
@@ -2,28 +2,36 @@ import 'package:campominado/models/campo.dart';
 import 'package:flutter/material.dart';
 
   class Tabuleiro extends StatelessWidget {
-    const Tabuleiro({super.key});
+    Tabuleiro({super.key});
 
-    final int _linhas = 20; //Define o número de linhas para o tabuleiro
+    //final int _linhas = 20; //Define o número de linhas para o tabuleiro
     final int _colunas = 20; //Define o número de colunas para o tabuleiro
+    int columnCount = 0;
   
     @override
     Widget build(BuildContext context) {
       Size screenSize = MediaQuery.of(context).size;
 
+      double proporcao = screenSize.width*0.8/screenSize.height;
+      print(proporcao);
+      print("Altura: ${screenSize.height}");
+      print("Largura: ${screenSize.width*0.8}");
+
   return Container(
     height: screenSize.height,
-    width: screenSize.width,
+    width: screenSize.width*0.8,
     color: Colors.grey[200],
-    //padding: const EdgeInsets.all(16.0),
-    child: GridView.count(
-      crossAxisCount: _colunas,
-      children: <Widget>[
-        for (var i = 0; i < _linhas; i++) 
-          for (var j = 0; j < _colunas; j++)
-            Campo()
-      ],
+    padding: const EdgeInsets.all(8.0),
+    child: GridView.builder(
+      shrinkWrap: true,
+     // physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: _colunas,
+      ),
+      itemBuilder: (context, position) {
+        return Campo();
+      },
     ),
   );
-}
+    }
   }
