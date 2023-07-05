0501cd18201ce359c4b30f78cff12662763b5c0f
joao <joaoabreugabriel@gmail.com>
2023-07-05 12:54:59 -0300

    retirado anotações desnecessárias e efeitos sonoros

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-//import 'package:audioplayers/audioplayers.dart';
 import 'package:condomine/screens/main_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-//final player = AudioPlayer();
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MaterialApp(
@@ -11,6 +9,4 @@ void main() {
       home: MainMenu(),
       title: 'Condomine',
       debugShowCheckedModeBanner: false));
-      //player.play(DeviceFileSource('assets/sons/condominesoudtrack.mp3'));
-      //player.setVolume(0.15);
 }
